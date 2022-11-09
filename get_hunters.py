d0d8495187d75227b22095e87e52777330299850
JanneL <97241078+LehtJan@users.noreply.github.com>
2022-11-09 14:35:41 +0200

    keskiviikon 9.11 aikaansaannoksia

--- get_hunters.py
+++ get_hunters.py
@@ -1,46 +1,37 @@
-# GET COLUMN NAMES FROM A TABLE
+# TEST THE CONNECTION TO POSTGRESQL SERVER ON LOCALHOST
 
 # LIBRARIES AND MODULES
 import psycopg2  # For PostgreSQL
 
-# Propetries of the connection string
+# Properties of the connection string
 database = "metsastys"
 user = "postgres"
 password = "Q2werty"
 host = "localhost"
 port = "5432"
 
-# Trying to establish a connection to DB server
+# Try to establish a connection to DB server
 try:
     # Create a connection object
     dbaseconnection = psycopg2.connect(database=database, user=user, password=password,
-                                       host=host, port=port)
-
+                                      host=host, port=port)
+    
     # Create a cursor to execute commands and retrieve result set
     cursor = dbaseconnection.cursor()
     
     # Execute a SQL command to get hunters (jasen)
     command = "SELECT * FROM public.jasen;"
     cursor.execute(command)
+    result_set = cursor.fetchall()
+    print("Jäsentiedot ovat:", result_set)
 
-    # Get number of rows in the result set
-    rows = cursor.rowcount
-    print("Tulosjoukossa on", rows, "riviä")
-    # Get column names from cursor
-    column_names =  [column_name[0] for column_name in cursor.description]
-    print("Sarakkeiden nimet ovat:")
-    for column in column_names:
-        print(column)
-    print("Sarakeluettelo päättyi")
-    
+# Throw an error if connection or cursor creation fails                                     
+except(Exception, psycopg2.Error) as e:
+    print("Tietokantayhteydessä tapahtui virhe", e)
 
-# Throw an error if connection or cursor creation fails    
-except (Exception, psycopg2.Error) as e:
-    print("Tietokantayhteydessä tapahtui virhe", e) 
-
-# If or if not successfull close the cursor and the connection    
+# If or if not successfull close the cursor and the connection   
 finally:
-    if (dbaseconnection):
+    if dbaseconnection:
         cursor.close()
         dbaseconnection.close()
         print("Yhteys tietokantaan katkaistiin")
--- groupInfoMainWindow.py
+++ groupInfoMainWindow.py
@@ -1,13 +1,14 @@
-# APPLICATUON FOR SHOWING SUMMARY DATA ABOUT MEAT GIVEN SHARE GROUP
-# ==================================================================
+# APPLICATON FOR SHOWING SUMMARY DATA ABOUT MEAT GIVEN TO SHARE GROUP
+# ====================================================================
 
 # LIBRARIES AND MODULES
 # ---------------------
 
 import sys # Needed for starting the application
-import psycopg2
-from PyQt5.QtWidgets import * # All widgets (*)
-from PyQt5.uic import loadUi # For loading the UI from a .ui file
+from PyQt5.QtWidgets import * # All widgets
+from PyQt5.uic import loadUi
+import pgModule
+import prepareData
 
 # CLASS DEFINITIONS FOR THE APP
 # -----------------------------
@@ -18,71 +19,65 @@ class GroupMainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
-        # Create an UI from the .ui file
+        # Create an UI from the ui file
         loadUi('groupInfoMainWindow.ui', self)
 
         # Define properties for ui elements
         self.refreshBtn = self.refreshPushButton
         self.groupInfo = self.groupSummaryTableWidget
         self.sharedMeatInfo = self.meatSharedTableWidget
-
+        '''
         # Database connection parameters
         self.database = "metsastys"
-        self.username = "sovellus"
+        self.user = "sovellus"
         self.userPassword = "Q2werty"
         self.server = "localhost"
         self.port = "5432"
-
-        # SIGNALS 
+        '''
+        # SIGNALS
 
         # Emit a signal when refresh push button is pressed
-        self.refreshBtn.clicked.connect(self.refreshData)
+        self.refreshBtn.clicked.connect(self.agentRefreshData)
 
     # SLOTS
 
-    # Load data to table Widgets
+    # Agent method is for receiving signals from an UI element
 
-    # Trying to establish a connection to DB server
-    def refreshData(self):
+    def agentRefreshData(self):
 
-        # To avoid fatal errors crashing the app uses try-except-finally structure 
-        try:
-            # Create a connection object
-            dbaseconnection = psycopg2.connect(database=self.database, user=self.username, password=self.userPassword,
-                                            host=self.server, port=self.port)
+        # Read data from view jaetut_lihat
+        databaseOperation1 = pgModule.DatabaseOperation()
+        connectionArguments = databaseOperation1.readDatabaseSettingsFromFile('settings.dat')
+        databaseOperation1.getAllRowsFromTable(connectionArguments, 'public.jaetut_lihat')
+        print(databaseOperation1.detailedMessage)
 
-            # Create a cursor to execute commands and retrieve result set
-            cursor = dbaseconnection.cursor()
-            
-            # Execute a SQL command to get hunters (jasen)
-            command = "SELECT * FROM public.jaetut_lihat;"
-            cursor.execute(command)
-            result_set = cursor.fetchall()
-            print("lihaa on jaettu seuraavasti:", result_set)         
+        # Read data from view jakoryhma_yhteenveto, no need to read connection arguments twice
+        databaseOperation2 = pgModule.DatabaseOperation()
+        databaseOperation2.getAllRowsFromTable(connectionArguments, 'public.jakoryhma_yhteenveto')
+        print(databaseOperation2.detailedMessage)
 
-        # Throw an error if connection or cursor creation fails    
-        except (Exception, psycopg2.Error) as e:
-            print("Tietokantayhteydessä tapahtui virhe", e) 
+        # Let's call the real method which updates the widget
+        self.refreshData(databaseOperation1, self.sharedMeatInfo)
+        self.refreshData(databaseOperation2, self.groupInfo)
+        
 
-        # If or if not successfull close the cursor and the connection    
-        finally:
-            if (dbaseconnection):
-                cursor.close()
-                dbaseconnection.close()
-                print("Yhteys tietokantaan katkaistiin")
+    # This is a function that updates table widgets in the UI, 
+    # because it doesn't receive signals: it's not a slot
+    def refreshData(self, databaseOperation, widget):
+        prepareData.prepareTable(databaseOperation, widget)
+
 
 # APPLICATION CREATION AND STARTING
-#----------------------------------
+# ----------------------------------
 
 # Check if app will be created and started directly from this file
 if __name__ == "__main__":
+
     # Create an application object
     app = QApplication(sys.argv)
     app.setStyle('Fusion')
 
     # Create the Main Window object from FormWithTable Class and show it on the screen
     appWindow = GroupMainWindow()
-    appWindow.show() # This can also be included in the FormWithTable
+    appWindow.show()  # This can also be included in the FormWithTable class
     sys.exit(app.exec_())
-
-# -----END OF APPLICATION BLOCK-----
